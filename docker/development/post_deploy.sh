cdb51578385698d4b1b60a42e75412296bb197ef
Jairo Matos da Rochal <devjairomr@gmail.com>
2023-07-28 10:30:59 -0300

    docker and path

---
+++ docker/development/post_deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+. /var/www/.env
+ssh -p 2522 -fN root@$SERVER -L 5432:127.0.0.1:5432
+ssh -p 2522 -fN root@$SERVER -L 27017:127.0.0.1:27017
+
+
+# update application cache
+php artisan optimize
+php artisan optimize:clear
+
+# start the application
+
+php-fpm -D &&  nginx -g "daemon off;"
