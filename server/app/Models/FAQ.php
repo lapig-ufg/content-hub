51ecaf7c2205e74b4d359155d8f672c3a126dd15
James-jamames <tiagogonsalves2001@gmail.com>
2024-01-15 16:03:10 -0300

    + FAQ database and BREAD

---
+++ server/app/Models/FAQ.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Traits\Translatable;
+
+class FAQ extends Model
+{
+    use HasFactory, Translatable;
+
+    protected $translatable = ['question', 'answer'];
+    protected $table = "faq";
+
+    protected $fillable = [
+        'question',
+        'answer',
+    ];
+
+    public static function getTranslate(string $lang)
+    {
+        $faq = FAQ::withTranslation($lang)->get();
+
+        foreach($faq as $element)
+        {
+            foreach($element->translations as $translation)
+            {
+                $element[$translation->column_name] = $translation->value;
+            }
+
+            unset($element->translations);
+        }
+
+        return $faq;
+    }
+}
